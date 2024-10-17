da175190dda75b1eb2b163a5d15f61cc73e25d12
sha-2701 <29800csaiml@gmail.com>
2024-10-17 15:03:48 +0530

    Second Day of Learning

--- source.py
+++ source.py
@@ -1,6 +1,31 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+st.title("My first app")
+number = st.slider("Select a number",0,1000,100) #Adding Slider to take input from user
+st.write(number)
+# Adding Button
+if st.button("Say Hello"):
+    st.write("bye")
+else:
+    st.write("Hello")
+
+#Adding Checkbox
+st.radio("Please select one",("1st Year","2nd Year","3rd Year","4th Year"))
+
+# Adding Dropdown List
+st.selectbox("Please select",(1,2,3,4))
+
+# Adding Selection Slider
+st.select_slider("Please select",(1,2,3,4,7))
+
+#SideBar
+st.sidebar.selectbox("Please select",(1,2,3,4,7))
+
+st.text_input("Enter your name")
+
+
+st.sidebar.text_input("Enter your Surname")
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
